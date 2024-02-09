ab8fa3f73ac78a9137fe7adf652a0bd195074408
Isaacson Tyimanda <144861805+isaacsontyimanda@users.noreply.github.com>
2024-02-10 00:28:23 +0100

    adicionei favicon

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function Ver() {
-  let img = document.getElementById('img')
   let nome = document.querySelector('input#instext')
   let res = document.getElementById('res')
   if (nome.value.length == 0) {
@@ -16,9 +15,12 @@ function Ver() {
       <li><p>Estabeleça metas que você pode alcançar, pois se a gente fica querendo o impossível, a frustração cedo ou tarde bate na porta. Decida o que você não quer mais na sua vida. Esse é um bom jeito de abrir espaço para tudo aquilo que você sonha. Ou tudo que você nem sabe que deseja.</p></li>
       <li><p>Ninguém nunca conseguiu alcançar sucesso simplesmente fazendo o que lhe é solicitado. É a quantidade e a excelência do que está além do solicitado que determina a grandeza da distinção final.</p></li>
       <li><p>Por último vai essa: "Você nunca alcança o sucesso verdadeiro a menos que você goste do que está fazendo."</p></li>
-      <li><abbr title="Volte a sessão anterior"><a class="links" href="index.html">Voltar</a></abbr></li>
       </ul>
     </p>`
+    document.querySelector('p.p1').style.display = 'none'
+    document.querySelector('input.bot').style.display = 'none'
+    document.getElementById('img').style.display = 'none'
     nome.value = ''
+
   }
 }
